3f3e30d20cb915e82614b53533d5040372ae9020
MyAlexro <44710632+MyAlexro@users.noreply.github.com>
2024-06-10 13:29:13 +0200

    Update main.c
    Updated animation and reduced mem usage

--- main.c
+++ main.c
@@ -17,20 +17,20 @@
 #define COL5 5
 #define COL6 4
 
-const short row[ROWS] = { ROW1, ROW2, ROW3, ROW4, ROW5, ROW6 };
-const short col[COLS] = { COL1, COL2, COL3, COL4, COL5, COL6 };
+const uint8_t row[ROWS] = { ROW1, ROW2, ROW3, ROW4, ROW5, ROW6 };
+const uint8_t col[COLS] = { COL1, COL2, COL3, COL4, COL5, COL6 };
 
 typedef struct {
-  short matrix[ROWS][COLS];
-  int frameDuration;
+  uint8_t matrix[ROWS][COLS];
+  uint16_t frameDuration;
 } frame;
 
 typedef struct {
-  frame frames[10];
-  short framesNumber;
+  frame frames[12];
+  uint8_t framesNumber;
 } animation;
 
-short buff_matrix[ROWS][COLS] = {
+uint8_t buff_matrix[ROWS][COLS] = {
   { 0, 0, 0, 0, 0, 0 },
   { 0, 0, 0, 0, 0, 0 },
   { 0, 0, 0, 0, 0, 0 },
@@ -40,17 +40,40 @@ short buff_matrix[ROWS][COLS] = {
 };
 
 uint32_t prev_millis = 0;
-short counter = 0;
+char counter = 0;
 
-void drawMatrix(short[ROWS][COLS]);
+void drawMatrix(uint8_t[ROWS][COLS]);
 void resetMatrix();
 void resetBufferMatrix();
-void updateBufferMatrix(short[ROWS][COLS]);
-void activateColumn(short);
+void updateBufferMatrix(uint8_t[ROWS][COLS]);
+void activateColumn(uint8_t);
 
 #pragma region animation frames
-
+frame f0 = { .matrix = {
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 } },
+             .frameDuration = FRAME_DURATION };
 frame f1 = { .matrix = {
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 1, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 } },
+             .frameDuration = FRAME_DURATION };
+frame f2 = { .matrix = {
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 1, 1, 1, 0, 0 },
+               { 0, 0, 1, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 },
+               { 0, 0, 0, 0, 0, 0 } },
+             .frameDuration = FRAME_DURATION };
+frame f3 = { .matrix = {
                { 0, 1, 0, 1, 0, 0 },
                { 1, 1, 1, 1, 1, 0 },
                { 0, 1, 1, 1, 0, 0 },
@@ -58,7 +81,7 @@ frame f1 = { .matrix = {
                { 0, 0, 0, 0, 0, 0 },
                { 0, 0, 0, 0, 0, 0 } },
              .frameDuration = FRAME_DURATION };
-frame f8 = { .matrix = {
+frame f4 = { .matrix = {
                { 0, 1, 0, 1, 0, 0 },
                { 1, 0, 1, 0, 1, 0 },
                { 0, 1, 0, 1, 0, 0 },
@@ -66,7 +89,7 @@ frame f8 = { .matrix = {
                { 0, 0, 0, 0, 0, 0 },
                { 0, 0, 0, 0, 0, 0 } },
              .frameDuration = FRAME_DURATION * 3 };
-frame f10 = { .matrix = {
+frame f5 = { .matrix = {
                 { 0, 1, 0, 1, 0, 0 },
                 { 1, 1, 1, 1, 1, 0 },
                 { 0, 1, 1, 1, 0, 0 },
@@ -74,7 +97,7 @@ frame f10 = { .matrix = {
                 { 0, 0, 0, 0, 0, 0 },
                 { 0, 0, 0, 0, 0, 0 } },
               .frameDuration = FRAME_DURATION * 2 };
-frame f2 = { .matrix = {
+frame f6 = { .matrix = {
                { 0, 0, 1, 0, 1, 0 },
                { 0, 1, 1, 1, 1, 1 },
                { 0, 0, 1, 1, 1, 0 },
@@ -82,7 +105,7 @@ frame f2 = { .matrix = {
                { 0, 0, 0, 0, 0, 0 },
                { 0, 0, 0, 0, 0, 0 } },
              .frameDuration = FRAME_DURATION };
-frame f3 = { .matrix = {
+frame f7 = { .matrix = {
                { 0, 0, 0, 0, 0, 0 },
                { 0, 0, 1, 0, 1, 0 },
                { 0, 1, 1, 1, 1, 1 },
@@ -90,7 +113,7 @@ frame f3 = { .matrix = {
                { 0, 0, 0, 1, 0, 0 },
                { 0, 0, 0, 0, 0, 0 } },
              .frameDuration = FRAME_DURATION };
-frame f4 = { .matrix = {
+frame f8 = { .matrix = {
                { 0, 0, 0, 0, 0, 0 },
                { 0, 0, 0, 0, 0, 0 },
                { 0, 0, 1, 0, 1, 0 },
@@ -99,7 +122,7 @@ frame f4 = { .matrix = {
                { 0, 0, 0, 1, 0, 0 } },
              .frameDuration = FRAME_DURATION };
 
-frame f5 = { .matrix = {
+frame f9 = { .matrix = {
                { 0, 0, 0, 0, 0, 0 },
                { 0, 0, 0, 0, 0, 0 },
                { 0, 1, 0, 1, 0, 0 },
@@ -107,7 +130,7 @@ frame f5 = { .matrix = {
                { 0, 1, 1, 1, 0, 0 },
                { 0, 0, 1, 0, 0, 0 } },
              .frameDuration = FRAME_DURATION };
-frame f6 = { .matrix = {
+frame f10 = { .matrix = {
                { 0, 0, 0, 0, 0, 0 },
                { 0, 1, 0, 1, 0, 0 },
                { 1, 1, 1, 1, 1, 0 },
@@ -115,7 +138,7 @@ frame f6 = { .matrix = {
                { 0, 0, 1, 0, 0, 0 },
                { 0, 0, 0, 0, 0, 0 } },
              .frameDuration = FRAME_DURATION };
-frame f7 = { .matrix = {
+frame f11 = { .matrix = {
                { 0, 1, 0, 1, 0, 0 },
                { 1, 1, 1, 1, 1, 0 },
                { 0, 1, 1, 1, 0, 0 },
@@ -126,7 +149,7 @@ frame f7 = { .matrix = {
 
 #pragma endregion
 
-animation anim = { .frames = { f1, f8, f10, f2, f3, f4, f5, f6, f7 }, .framesNumber = 9 };
+animation anim = { .frames = { f0,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11 }, .framesNumber = 12 };
 
 frame frameProva = {
   .matrix = {
@@ -157,11 +180,11 @@ frame frameProva2 = {
 void setup() {
 
 
-  for (int i = 0; i < ROWS; i++) {
+  for (uint8_t i = 0; i < ROWS; i++) {
     pinMode(row[i], OUTPUT);
     digitalWrite(row[i], LOW);
   }
-  for (int i = 0; i < COLS; i++) {
+  for (uint8_t i = 0; i < COLS; i++) {
     pinMode(col[i], OUTPUT);
     digitalWrite(col[i], HIGH);
   }
@@ -202,7 +225,7 @@ void drawMatrix() {
   }
 }
 
-void activateColumn(short column) {
+void activateColumn(uint8_t column) {
   //Turn off all the other columns while turning the desired one on(OFF=HIGH, ON=LOW)
   for (int i = 0; i < COLS; i++) {
     digitalWrite(col[i], !(column == col[i]));
@@ -210,16 +233,16 @@ void activateColumn(short column) {
 }
 
 void resetMatrix() {
-  for (int i = 0; i < COLS; i++)
+  for (uint8_t i = 0; i < COLS; i++)
     digitalWrite(col[i], HIGH);
 
-  for (int i = 0; i < ROWS; i++)
+  for (uint8_t i = 0; i < ROWS; i++)
     digitalWrite(row[i], LOW);
 }
 
-void updateBufferMatrix(short matrix[ROWS][COLS]) {
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
+void updateBufferMatrix(uint8_t matrix[ROWS][COLS]) {
+  for (uint8_t i = 0; i < ROWS; i++) {
+    for (uint8_t j = 0; j < COLS; j++) {
       buff_matrix[i][j] = matrix[i][j];
     }
   }
